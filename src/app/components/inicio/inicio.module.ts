b8d4500d61b542a877f074d462b9018c00a7dc0f
zenTCS <jonathan.zheng@tcs.com.ve>
2023-02-03 14:12:00 -0400

    add slides

--- src/app/components/inicio/inicio.module.ts
+++ src/app/components/inicio/inicio.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { InicioPageRoutingModule } from './inicio-routing.module';
-
 import { InicioPage } from './inicio.page';
 
 @NgModule({
